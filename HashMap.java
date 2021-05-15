ca34bc8df71fca7b54d6a73e2fddf998dbeeaa1a
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2021-05-15 17:06:57 -0600

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -4,20 +4,75 @@ Algoritmos y Estructura de Datos
 Hoja de Trabajo 9
 Natanael Girón (20029)
 Ma. Isabel Solano (20504)
-Clase Factory
+Clase HashMap
 **/
 
-public class HashMap{
-	
-  public void put(K key, V value) {
-    
+import java.util.ArrayList;
+
+public class HashMap<Key extends Comparable<Key>, Value> implements SearchInterface<Key, Value> {
+
+	private ArrayList<Key> keys = new ArrayList<Key>();
+	private ArrayList<Value> values = new ArrayList<Value>();
+
+	@Override
+	public void put(Key key, Value value) {
+		keys.add(key);
+		values.add(value);
+	}
+
+	@Override
+	public boolean contains(Key key) {
+		boolean result = keys.contains(key);
+		return result;
+	}
+
+	@Override
+	public Value get(Key key) {
+		int index = -1;
+
+		// Búsqueda del indice de la llave.
+		for (int i = 0; i < keys.size(); i++) {
+			if (keys.get(i).equals(key)) {
+				index = i;
+			}
+		}
+
+		// Retorno del valor agregado.
+		if (index != -1) {
+			Value value = values.get(index);
+			return value;
+		}
+
+		return null;
+	}
+
+	public ArrayList<Key> getKeys() {
+		return keys;
+	}
+
+  public int size() {
+    return keys.size();
   }
 
-  public boolean containsKey(K key) {
-    
-  }
 
-  public V get(K key) {
+	public Value remove(Key key) {
+		int index = -1;
 
-   
+		// Búsqueda del indice de la llave.
+		for (int i = 0; i < keys.size(); i++) {
+			if (keys.get(i).equals(key)) {
+				index = i;
+				keys.remove(i);
+			}
+		}
+
+		// Retorno del valor removido.
+		if (index != -1) {
+			Value value = values.remove(index);
+			return value;
+		}
+
+		return null;
+	}
+
 }
